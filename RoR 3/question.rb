8716bd2d4a5683ae4994b9e27170ba8c7571a353
David Duke <drduke1@gmail.com>
2014-04-25 13:57:15 -0700

    Added Rails 4 files

---
+++ RoR 3/question.rb
@@ -0,0 +1,18 @@
+class Question < ActiveRecord::Base
+	has_many :answers, dependent: :destroy
+	belongs_to :user
+  accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
+    
+	#default_scope -> { order('created_at ASC') }
+	
+  @category_check = ['ip_voice', 'ip_pbx', 'ip_video_surveillance', 'ip_video_telephony', 'consumer_atas', 'enterprise_gateways']  
+    
+  validates :product_id, presence: true
+  validates :content, length: { maximum: 1000 }
+  validates_inclusion_of :category, in: @category_check
+      
+   # private
+   # def self.category_check
+   #     @@category_check = ['ip_voice', 'ip_pbx', 'ip_video_surveillance', 'ip_video_telephony', 'consumer_atas', 'enterprise_gateways']
+   # end
+end
